3550befbb15a7d655d3566347f3c375776f5b38e
Your Name <CHANNY.CHHUN@PNC.LAN>
2024-02-23 10:13:36 +0700

    [feat]change formedit #GEL9-51

--- views/category/displayCategory.view.php
+++ views/category/displayCategory.view.php
@@ -50,7 +50,44 @@
             <th scope="row"><?= $category['cat_id'] ?></th>
             <td><?= $category['cateName'] ?></td>
             <td><a href="controllers/category/deleteCategory.controller.php?id=<?=$category['cat_id'] ?>"><i class="fa fa-trash" style="color:red;"></i></a>
-            <a href="views/category/editCategory.view.php?id=<?=$category['cat_id'] ?>"><i class="fa fa-edit" style="color:blue;"></i></a>
+            <i class="fas fa-edit editIcon" data-toggle="modal" data-target="#editModal<?=$category['cat_id'] ?>" style="cursor: pointer;color:blue;"></i>
+            <div class="modal fade" id="editModal<?=$category['cat_id'] ?>" tabindex="-1" role="dialog" aria-labelledby="editModalLabel<?=$category['cat_id'] ?>" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content" style="background-color: black; border: 1px solid white;">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="editModalLabel">Edit Category</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <!-- <?php 
+                      // $id=$_GET['id'];
+                      // $statement = $connection->prepare("select * from category where cat_id=:id");
+                      // $statement->execute([':id' => $id]);
+                      // $category=$statement->fetch();
+                    ?> -->
+
+                    <div class="modal-body">
+                    <form action="controllers/category/updateCategory.controller.php" method="post">
+                <div class="form-group">
+                    <label for="category">Name Category:</label>
+                    <input type="hidden" name="id"
+                     value="<?= $category['cat_id'] ?>">
+                <div class="form-group">
+                    <input type="text" class="form-control bg-white"
+                     value="<?=$category['cateName']?>" name="cateName">
+              </div>
+        </div>
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    
+                    <button type="submit"class="btn btn-danger">Update</button>
+                </div>
+            </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
           </tr>
         <?php endforeach ?>
       </tbody>
