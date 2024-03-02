ebfe73784df9594744c479bf3c11bd7cc5779e09
Leak <vorleak.kun@student.passerellesnumeriques.org>
2024-03-02 10:31:22 +0700

    merge display course in each category

--- controllers/courses/displayAllcourse.controller.php
+++ controllers/courses/displayAllcourse.controller.php
@@ -1,8 +1,5 @@
 <?php
-// Start the session
-// session_start();
 
-// Check if the session variable is set
 if(isset($_SESSION['displayCourse'])){
     // Retrieve the value from the session
     $displayCourses = $_SESSION['displayCourse'];
