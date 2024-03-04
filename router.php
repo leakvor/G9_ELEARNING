68ee5083a5a35fd63dbd4eba9918f841e8dd2637
Ngor <NG.MAM@PNC.LAN>
2024-03-04 09:31:47 +0700

    template paymenttrainer

--- router.php
+++ router.php
@@ -21,6 +21,7 @@ if(isset($_SESSION['user'])||isset($_SESSION['teacher'])){
         '/signups' => 'controllers/signup/signup.controler.php',
         '/signins' => 'controllers/signin/signin.controller.php',
         '/trainer' => 'controllers/trainers/signinTrainer.controller.php',
+        '/templatePaymentTranier' => 'controllers/trainers/templatePaymentTrainer.controller.php',
         // '/trainerdashboard' => 'controllers/trainers/trainerDashboard.controller.php',
     ];
 }
