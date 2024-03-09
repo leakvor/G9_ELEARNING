282ef9c6d7cb19faa350c4958ae61378e7078a17
SENRIN.SIM <SENRIN.SIM@PNC.LAN>
2024-03-09 10:24:03 +0700

    save data when user join class to databases #GEL9-140

--- controllers/students/student.course.controller.php
+++ controllers/students/student.course.controller.php
@@ -10,14 +10,12 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
     $recordStuCou = getcourse_student($user_id, $course_id);
 
     if (count($recordStuCou) > 0){
-        // $_SESSION['StuRecCou'] = $recordStuCou;
-        var_dump($recordStuCou);
+        $_SESSION['StuRecCou'] = $recordStuCou;
         header('Location: /displayAllcourse');
     }else{
-        // courseStudent($user_id, $course_id, $date_join); 
-        // $_SESSION['stu_id'] = $user_id;
-        // $_SESSION['cou_id'] = $course_id;
-        echo "joined";
+        courseStudent($user_id, $course_id, $date_join); 
+        $_SESSION['stu_id'] = $user_id;
+        $_SESSION['cou_id'] = $course_id;
         header('Location: /displayAllcourse');
     }
 }
