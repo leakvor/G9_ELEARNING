a0f280ce2df23a8872b0dae1bc588e4c6da7f521
Your Name <CHANNY.CHHUN@pnc.lan>
2024-03-15 07:30:22 +0700

    updateprofile student

--- views/students/updateprofile.view.php
+++ views/students/updateprofile.view.php
@@ -149,11 +149,6 @@ Page content START -->
             <div class="bg-dark border rounded-3 pb-0 p-3 w-100">
               <!-- Dashboard menu -->
               <div class="list-group list-group-dark list-group-borderless">
-                <!-- <a class="list-group-item" href="instructor-dashboard.html"><i class="bi bi-ui-checks-grid fa-fw me-2"></i>Dashboard</a> -->
-                <!-- <a class="list-group-item" href="instructor-manage-course.html"><i class="bi bi-basket fa-fw me-2"></i>My Category</a> -->
-                <!-- <a class="list-group-item" href="instructor-manage-course.html"><i class="bi bi-basket fa-fw me-2"></i>My Courses</a> -->
-                <!-- <a class="list-group-item" href="instructor-earning.html"><i class="bi bi-graph-up fa-fw me-2"></i>Earnings</a> -->
-                <!-- <a class="list-group-item" href="instructor-studentlist.html"><i class="bi bi-people fa-fw me-2"></i>Students</a> -->
                 <a class="list-group-item active" href="instructor-edit-profile.html"><i class="bi bi-pencil-square fa-fw me-2"></i>Edit Profile</a>
                 <a class="list-group-item" href="instructor-delete-account.html"><i class="bi bi-trash fa-fw me-2"></i>Delete Profile</a>
                 <a class="list-group-item text-danger bg-danger-soft-hover" href="sign-in.html"><i class="fas fa-sign-out-alt fa-fw me-2"></i>Sign Out</a>
@@ -202,7 +197,7 @@ Page content START -->
                 <label class="form-label">Username</label>
                 <div class="input-group">
                   <!-- <span class="input-group-text">Eduport.com</span> -->
-                  <input type="text" class="form-control" value="<?= $user['username'] ?>">
+                  <input type="text" class="form-control" value="<?= $user['username'] ?>" name="username">
                 </div>
               </div>
 
