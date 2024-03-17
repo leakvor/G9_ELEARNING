e24fd786d1b84ed1f8a1db2fbca40df8042ab560
Your Name <CHANNY.CHHUN@pnc.lan>
2024-03-17 20:29:05 +0700

    develops

--- database/database.php
+++ database/database.php
@@ -3,7 +3,7 @@
 $hostname = "localhost";
 $database = "g9_elearning";
 $username = "root";
-$password = "root";
+$password = "";
 
 $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
 $connection = new PDO($dsn, $username, $password);
