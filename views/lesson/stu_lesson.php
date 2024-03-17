0ef47af88b36d57e5db44118946f9de04c2a1c7d
Your Name <CHANNY.CHHUN@pnc.lan>
2024-03-17 19:46:34 +0700

    [ comment_course]

--- views/lesson/stu_lesson.php
+++ views/lesson/stu_lesson.php
@@ -180,10 +180,94 @@ Page Banner START -->
 							</ol>
 						</nav>
 					</div>
+					<!-- ===========================comment================= -->
+						<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Comment</a>
+						<div class="dropdown-menu dropdown-menu-end" data-bs-popper="none" style="width:100%">
+							<div class="row p-2">
+									<div class="card shadow-0 border" style=" background-color:#E0E0E0;width:95%; margin-left: 30px;">
+										<div class="form-outline mb-4" style="width:100%;">
+										<input type="text" id="ddANote" class="form-control" placeholder="Type comment...">
+										<button type="submit" style="margin:10px; padding:5px ;border:none;border-radius:5px">+ Add a note</button>
+										
+										<div class="card mb-5">
+										<div class="card-body">
+											<p>Type your note, and hit enter to add it</p>
+
+											<div class="d-flex justify-content-between">
+											<div class="d-flex flex-row align-items-center">
+												<img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp" alt="avatar" width="25" height="25">
+												<p class="small mb-0 ms-2">Martha</p>
+											</div>
+											<div claass="d-flex flex-row align-items-center">
+												<p class="small text-muted mb-0">Upvote?</p>
+												<i class="far fa-thumbs-up mx-2 fa-xs text-black" style="margin-top: -0.16rem;"></i>
+												<p class="small text-muted mb-0">3</p>
+											</div>
+											</div>
+										</div>
+										</div>
+
+										<div class="card mb-4">
+										<div class="card-body">
+											<p>Type your note, and hit enter to add it</p>
+
+											<div class="d-flex justify-content-between">
+											<div class="d-flex flex-row align-items-center">
+												<img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar" width="25" height="25">
+												<p class="small mb-0 ms-2">Johny</p>
+											</div>
+											<div class="d-flex flex-row align-items-center">
+												<p class="small text-muted mb-0">Upvote?</p>
+												<i class="far fa-thumbs-up mx-2 fa-xs text-black" style="margin-top: -0.16rem;"></i>
+												<p class="small text-muted mb-0">4</p>
+											</div>
+											</div>
+										</div>
+										</div>
+
+										<div class="card mb-4">
+										<div class="card-body">
+											<p>Type your note, and hit enter to add it</p>
+
+											<div class="d-flex justify-content-between">
+											<div class="d-flex flex-row align-items-center">
+												<img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp" alt="avatar" width="25" height="25">
+												<p class="small mb-0 ms-2">Mary Kate</p>
+											</div>
+											<div class="d-flex flex-row align-items-center text-primary">
+												<p class="small mb-0">Upvoted</p>
+												<i class="fas fa-thumbs-up mx-2 fa-xs" style="margin-top: -0.16rem;"></i>
+												<p class="small mb-0">2</p>
+											</div>
+											</div>
+										</div>
+										</div>
+
+										<div class="card">
+										<div class="card-body">
+											<p>Type your note, and hit enter to add it</p>
+
+											<div class="d-flex justify-content-between">
+											<div class="d-flex flex-row align-items-center">
+												<img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar" width="25" height="25">
+												<p class="small mb-0 ms-2">Johny</p>
+											</div>
+											<div class="d-flex flex-row align-items-center">
+												<p class="small text-muted mb-0">Upvote?</p>
+												<i class="far fa-thumbs-up ms-2 fa-xs text-black" style="margin-top: -0.16rem;"></i>
+											</div>
+											</div>
+										</div>
+										</div>
+									</div>
+									</div>
+								</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	</div>
 </section>
 
 
