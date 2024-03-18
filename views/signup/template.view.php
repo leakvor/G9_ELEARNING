f22afaaeb184a0cad5ba479a6c23cfc0744664dd
maisamorn <samorn.mai@student.passerellesnumeriques.org>
2024-03-18 11:35:06 +0700

    form

--- views/signup/template.view.php
+++ views/signup/template.view.php
@@ -1,31 +1,28 @@
-<main>
-	<section class="p-0 d-flex align-items-center position-relative overflow-hidden">
-	
-		<div class="container-fluid">
-			<div class="row">
+<header>
+	<div class="d-flex">
 				<!-- left -->
-				<div class="col-12 col-lg-6 d-md-flex align-items-center justify-content-center bg-primary bg-opacity-10 vh-lg-100">
-					<div class="p-3 p-lg-5">
+		<div class="col-12 col-lg-6 d-md-flex align-items-center justify-content-center bg-primary bg-opacity-10 vh-lg-100">
+			<div class="p-3 p-lg-5">
 						<!-- Title -->
-						<div class="text-center">
-							<h2 class="fw-bold">Welcome to our largest community</h2>
-							<p class="mb-0 h6 fw-light">Let's learn something new today!</p>
-						</div>
-						<!-- SVG Image -->
-						<img src="assets/images/element/02.svg" class="mt-5" alt="">
-						<!-- Info -->
-						<div class="d-sm-flex mt-5 align-items-center justify-content-center">
-							<ul class="avatar-group mb-2 mb-sm-0">
-								<li class="avatar avatar-sm"><img class="avatar-img rounded-circle" src="assets/images/avatar/01.jpg" alt="avatar"></li>
-								<li class="avatar avatar-sm"><img class="avatar-img rounded-circle" src="assets/images/avatar/02.jpg" alt="avatar"></li>
-								<li class="avatar avatar-sm"><img class="avatar-img rounded-circle" src="assets/images/avatar/03.jpg" alt="avatar"></li>
-								<li class="avatar avatar-sm"><img class="avatar-img rounded-circle" src="assets/images/avatar/04.jpg" alt="avatar"></li>
-							</ul>
-							<!-- Content -->
-							<p class="mb-0 h6 fw-light ms-0 ms-sm-3">4k+ Students joined us, now it's your turn.</p>
-						</div>
-					</div>
+				<div class="text-center">
+					<h2 class="fw-bold">Welcome to our largest community</h2>
+					<p class="mb-0 h6 fw-light">Let's learn something new today!</p>
 				</div>
+						<!-- SVG Image -->
+				<img src="assets/images/element/02.svg" class="mt-5" alt="">
+						<!-- Info -->
+				<div class="d-sm-flex mt-5 align-items-center justify-content-center">
+					<ul class="avatar-group mb-2 mb-sm-0">
+						<li class="avatar avatar-sm"><img class="avatar-img rounded-circle" src="assets/images/avatar/01.jpg" alt="avatar"></li>
+						<li class="avatar avatar-sm"><img class="avatar-img rounded-circle" src="assets/images/avatar/02.jpg" alt="avatar"></li>
+						<li class="avatar avatar-sm"><img class="avatar-img rounded-circle" src="assets/images/avatar/03.jpg" alt="avatar"></li>
+						<li class="avatar avatar-sm"><img class="avatar-img rounded-circle" src="assets/images/avatar/04.jpg" alt="avatar"></li>
+					</ul>
+							<!-- Content -->
+					<p class="mb-0 h6 fw-light ms-0 ms-sm-3">4k+ Students joined us, now it's your turn.</p>
+				</div>
+			</div>
+		</div>
 
 				<!-- Right -->
 				<div class="col-12 col-lg-6 m-auto">
@@ -103,6 +100,6 @@
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-</main>
+	
+
+</header>
